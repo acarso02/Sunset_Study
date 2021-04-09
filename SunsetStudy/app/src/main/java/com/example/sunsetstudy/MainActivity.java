2657d40122e4c2b1d161fabc7619e8b6dd93b2f3
Jonah Ruch <jonah.ruch@gmail.com>
2021-04-09 13:18:12 -0400

    navigation drawer now links to all projects(main screen) and addNewCard screen

--- SunsetStudy/app/src/main/java/com/example/sunsetstudy/MainActivity.java
+++ SunsetStudy/app/src/main/java/com/example/sunsetstudy/MainActivity.java
@@ -5,18 +5,21 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Button addButton;
     private Toolbar toolbar;
     private NavigationView navigationView;
@@ -30,12 +33,17 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.nav_view);
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        // toolbar
         setSupportActionBar(toolbar);
 
-
+        // nav drawer menu
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
 
 //        addButton = (Button) findViewById(R.id.add_question_button);
 
@@ -60,6 +68,27 @@ public class MainActivity extends AppCompatActivity {
 //                openActivityAdd();
 //            }
 //        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.allProjects:
+                Intent home = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(home);
+                break;
+//            case R.id.newProject:
+//                Intent createProject = new Intent(MainActivity.this, Project.class);
+//                startActivity(createProject);
+//                break;
+            case R.id.addCard:
+                Intent createCard = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivity(createCard);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 //    public void openActivityAdd(){
 //        Intent i = new Intent(this, AddCardActivity.class);
